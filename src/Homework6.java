f5215aa6ae5a76a130cc11eba35cacd4a4589221
JuHeon <juheon0313@naver.com>
2025-11-11 10:30:34 +0900

    ✨ feat: 7-1 과제

---
+++ src/Homework6.java
@@ -0,0 +1,45 @@
+class Course {
+    private String code;
+    private String name;
+
+    public Course(String code, String name){
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode(){
+        return this.code;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
+
+class OnlineCourse extends Course {
+    public OnlineCourse(String code, String name){
+        super(code, name);
+    }
+
+    public String toString(){
+        return "Code: "+super.getCode()+", Name: "+super.getName()+", Type: Online";
+    }
+}
+
+class OfflineCourse extends Course {
+    public OfflineCourse(String code, String name){
+        super(code, name);
+    }
+
+    public String toString(){
+        return "Code: "+super.getCode()+", Name: "+super.getName()+", Type: Offline";
+    }
+}
+public class Homework6 {
+    public static void main(String[] args) {
+        OfflineCourse course1 = new OfflineCourse("HAEA9201", "Object Oriented Programming");
+        OnlineCourse course2 = new OnlineCourse("HAFL0012", "C Programming 1");
+        System.out.println(course1);
+        System.out.println(course2);
+    }
+}
