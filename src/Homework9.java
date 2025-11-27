2147504803f063661ed0b336391d6702da676e95
JuHeon <juheon0313@naver.com>
2025-11-27 17:49:56 +0900

    ✨ feat: 9 과제

--- src/Homework9.java
+++ src/Homework9.java
@@ -1,23 +1,28 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 import static java.lang.System.exit;
 
-public class Homework8 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class Homework9 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(new File("src/db.txt"));
         Map<String, String> map = new HashMap<>();
 
-        map.put("myId", "myPass");
-        map.put("myId2", "myPass2");
-        map.put("myId3", "myPass3");
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine().trim();
+            String[] idPassword = line.split(":");
+            map.put(idPassword[0], idPassword[1]);
+        }
 
         while (true){
+            Scanner scanner2 = new Scanner(System.in);
             System.out.println("id와 password를 입력해주세요.");
             System.out.print("id : ");
 
-            String id = scanner.nextLine();
+            String id = scanner2.nextLine();
 
             if (!map.containsKey(id)) {
                 System.out.println("입력하신 id는 존재하지 않습니다. 다시 입력해주세요.");
@@ -25,7 +30,7 @@ public class Homework8 {
             }
 
             System.out.print("password : ");
-            String password = scanner.nextLine();
+            String password = scanner2.nextLine();
 
             if (!map.get(id).equals(password)) {
                 System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
